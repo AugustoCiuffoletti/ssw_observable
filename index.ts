b88e8b98def743451a9d571ecf5e03e310f76532
AugustoCiuffoletti <augusto@di.unipi.it>
2022-03-22 12:00:52 +0100

    1 file modified

--- index.ts
+++ index.ts
@@ -1,6 +1,6 @@
 import { Observable } from 'rxjs';
  
-const observable = new Observable(subscriber => {
+const observable: Observable<string> = new Observable(subscriber => {
   subscriber.next(1);
   subscriber.next(2);
   subscriber.next(3);
